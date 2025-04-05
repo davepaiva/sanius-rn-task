1f2ce052b029e704b78fe8b9f169a520112a18e0
Dave Paiva <mrdavepaiva@gmail.com>
2025-04-05 23:10:47 +0530

    ADD react query

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import RootStack from '@navigators/index';
-import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaView } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaView} from 'react-native';
 import globalStyles from '@styles/globalStyles';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import getMovieGenreList from '@api/GetMovieGenreList';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import asyncStorageKeys from '@app_utils/asynStorageKeys';
 import getTvGenreList from '@api/GetTvGenreList';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
+// Create a client
+const queryClient = new QueryClient();
 
 export default function App() {
-
   useEffect(() => {
     const fetchGenreList = async () => {
       try {
@@ -19,7 +21,10 @@ export default function App() {
         const tvGenreList = await getTvGenreList();
         const genreList = [...movieGenreList.genres, ...tvGenreList.genres];
         console.log('genreList', genreList);
-        AsyncStorage.setItem(asyncStorageKeys.movieGenres, JSON.stringify(genreList));
+        AsyncStorage.setItem(
+          asyncStorageKeys.movieGenres,
+          JSON.stringify(genreList),
+        );
       } catch (error) {
         console.error('Error fetching genre list:', error);
       }
@@ -28,16 +33,14 @@ export default function App() {
   }, []);
 
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={globalStyles.f1}>
-        <NavigationContainer>
-          <RootStack />
-        </NavigationContainer>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <SafeAreaView style={globalStyles.f1}>
+          <NavigationContainer>
+            <RootStack />
+          </NavigationContainer>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
-
-
-
-
